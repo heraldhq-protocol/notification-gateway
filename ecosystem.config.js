7e2290e466172f5683b9fc84b303585796a299ee
Adebayo Anuoluwa Success <adebayo.anuoluwa02@gmail.com>
2026-04-09 08:07:49 +0100

    fix: resolve issue in running start:prod

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,25 +1,25 @@
-module.exports = {
-  apps: [
-    {
-      name: 'herald-gateway',
-      script: 'dist/main.js',
-      instances: 1,
-      autorestart: true,
-      watch: false,
-      max_memory_restart: '512M',
-      env: {
-        NODE_ENV: 'production',
-      },
+export const apps = [
+  {
+    name: 'herald-gateway',
+    script: 'pnpm',
+    args: 'run start:prod',
+    instances: 1,
+    autorestart: true,
+    watch: false,
+    max_memory_restart: '512M',
+    env: {
+      NODE_ENV: 'production',
+      NITRO_ENCLAVE_SOCKET: '/home/ec2-user/notification-gateway/socket.sock',
     },
-    {
-      name: 'mock-enclave',
-      script: 'docker/mock-enclave.js',
-      instances: 1,
-      autorestart: true,
-      watch: false,
-      env: {
-        NITRO_ENCLAVE_SOCKET: '/run/enclave.sock',
-      },
+  },
+  {
+    name: 'mock-enclave',
+    script: 'docker/mock-enclave.js',
+    instances: 1,
+    autorestart: true,
+    watch: false,
+    env: {
+      NITRO_ENCLAVE_SOCKET: '/home/ec2-user/notification-gateway/socket.sock',
     },
-  ],
-};
+  },
+];
